a499375e1d6c6979bd76badb56108357ef5b73d3
Warley Oliveira <warley.oliveira@institutoaquila.com>
2017-07-16 19:26:50 -0300

    Primeiro teste funcional

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,14 @@
 class HomeController < ApplicationController
   def index
   end
+
+  def form
+  	name = params[:contact_us][:name]
+  	email = params[:contact_us][:email]
+  	messsage = params[:contact_us][:messsage] rescue ""
+  	ip = request.remote_ip
+
+  	Contact.create(nome: name, email: email, comentario: messsage, ip: ip)
+  	redirect_to root_path
+  end
 end
